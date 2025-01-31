ee91bc22fa1a716360ac7ad3f19242672f62e22f
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 22:00:42 +0300

    Update errors.js

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,6 +1,17 @@
+// middleware/errorMiddleware.js
 exports.globalErrorMiddleware = function (err, req, res, next) {
-    res.status(err.status || 500).json({
+    
+    const statusCode = err.status || 500;
+    const message = err.message || "Internal Server Error";
+    if (process.env.NODE_ENV === 'development') {
+        return res.status(statusCode).json({
+            success: false,
+            message: message,
+            stack: err.stack  // Muujinta stack trace si loo sahlo debugging
+        });
+    }
+    return res.status(statusCode).json({
         success: false,
-        message: err.message || "Internal Server Error"
-    })
-}
+        message: message
+    });
+};
