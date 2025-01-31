7bcfcae3d28babadf01a906f16fce52852d7277f
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 22:03:06 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const { login, register, forgetPassword, resetPassword } = require('../controllers/auth');
 
+
+
 router.post('/login', login);
-router.post('/register', register)
+router.post('/register', register);
 router.post('/forget-password', forgetPassword);
 router.post('/reset-password', resetPassword);
 
