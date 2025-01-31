745f086e772df308cef53289706f4ae8a5b6731f
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 21:58:43 +0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -2,12 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const URI = process.env.MONGO_URI;
-        const conn = await mongoose.connect(URI);
+       
+        const URI = process.env.MONGO_URI || "your_default_mongo_connection_string";  // Haddii aan la helin .env file
+        const conn = await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
-        console.error(err.message);
+        console.error("Error connecting to MongoDB:", err.message);
+        process.exit(1);  // Stop the process if DB connection fails
     }
-}
+};
 
 module.exports = connectDB;
