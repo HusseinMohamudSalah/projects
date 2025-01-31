2d55d49e69e1fc47f2c3d663324f0270d1ccfb4e
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 22:04:23 +0300

    Update mailer.js

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,25 +3,27 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (email, subject, message) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: process.env.EMAIL_SERVICE,
-            auth: {
-                user: process.env.EMAIL,
+            service: process.env.EMAIL_SERVICE,  
+                user: process.env.EMAIL,  
                 pass: process.env.PASSWORD,
             },
         });
 
+
         const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: subject,
-            text: message,
+            from: process.env.EMAIL, 
+            to: email,  
+            subject: subject,  
+            text: message,  
         };
 
+       
+
         await transporter.sendMail(mailOptions);
-        console.log('Email sent successfully');
+        console.log('Email successfully sent');
     } catch (error) {
         console.error('Error sending email:', error);
     }
 };
 
-module.exports = sendEmail;
+module.exports = sendEmail; 
