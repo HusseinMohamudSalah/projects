faf4c40d17ffe11af71c49468b6f1aca9519c7f1
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 22:03:31 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,18 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getProducts,
-    createProduct,
-    updateProduct,
-    deleteProduct
-} = require('../controllers/products');
+    getChats,
+    sendMessage,
+    updateMessage,
+    deleteMessage
+} = require('../controllers/chat');
 
 const { allowedRoles } = require('../middlewares/acl');
 const { guard } = require('../middlewares/guard');
 
-router.get('/', guard, allowedRoles('admin', 'user'), getProducts);
-router.post('/', guard, allowedRoles('admin'), createProduct);
-router.patch('/:id', guard, allowedRoles('admin'), updateProduct);
-router.delete('/', guard, allowedRoles('admin'), deleteProduct);
+
+router.get('/', guard, allowedRoles('admin', 'user'), getChats);
+router.post('/', guard, allowedRoles('admin', 'user'), sendMessage);
+router.patch('/:id', guard, allowedRoles('admin', 'user'), updateMessage);
+router.delete('/:id', guard, allowedRoles('admin', 'user'), deleteMessage);
+
 
 module.exports = router;
