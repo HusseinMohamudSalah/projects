6f822c5924c6f166eca8b9666979a3a1c3a3f6e5
HusseinMohamudSalah <husnicade9@gmail.com>
2025-01-31 21:59:52 +0300

    Update acl.js

--- middlewares/acl.js
+++ middlewares/acl.js
@@ -1,12 +1,15 @@
 exports.allowedRoles = (...roles) => {
     return (req, res, next) => {
-        if(!req.user) return res.status(401).json({ message: 'Unauthorized' });
-
-        if (!roles.includes(req.user.role)) {
-            console.log('You must be an admin', req.user.role, roles);
-            return res.status(403).json({ message: 'Unauthorized' })
+        if (!req.user) {
+            return res.status(401).json({ message: 'Unauthorized: No user found' });
         }
-
+        if (!roles.includes(req.user.role)) {
+            console.log('Unauthorized access attempt:', req.user.role, roles);
+            return res.status(403).json({ message: 'Forbidden: Insufficient permissions' });
+        }
         next();
-    }
-}
+    };
+};
+
+
+
